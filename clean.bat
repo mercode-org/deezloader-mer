fc4f6550420e90c083fbc4b0d3e0ab82d1ab4511
RemixDev <deezloaderremix@gmail.com>
2019-01-30 00:21:23 +0100

    Fixed #135

--- clean.bat
+++ clean.bat
@@ -1,12 +1,6 @@
 @echo off
 
 :: Deleting some garbage
-del yarn.lock /q >nul 2>nul
-del package-lock.json /q >nul 2>nul
-
-del app\package-lock.json /q >nul 2>nul
-del app\yarn.lock /q >nul 2>nul
-
 RD /S /Q node_modules >nul 2>nul
 RD /S /Q app\node_modules >nul 2>nul
 
--- clean.sh
+++ clean.sh
@@ -1,7 +1,3 @@
-rm -f yarn.lock
-rm -f package-lock.json
-rm -f app/package-lock.json
-rm -f app/yarn.lock
 rm -rf node_modules
 rm -rf app/node_modules
 rm -rf dist/*
