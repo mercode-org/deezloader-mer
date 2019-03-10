7bb0ed6a9434f04aebefa77e3298e7f0548b55fc
RemixDev <deezloaderremix@gmail.com>
2019-03-10 21:02:02 +0100

    Maybe fixed menu

--- app/main.js
+++ app/main.js
@@ -66,25 +66,6 @@ function loadSettings(){
 	}
 }
 
-var template = [{
-    label: "Application",
-    submenu: [
-        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-    ]}, {
-    label: "Edit",
-    submenu: [
-        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]}
-]
-
-Menu.setApplicationMenu(Menu.buildFromTemplate(template))
-
 function createTray(){
 	tray = new Tray(trayIcon);
 	const contextMenu = Menu.buildFromTemplate([
@@ -132,6 +113,24 @@ function createWindow () {
 	});
 
 	mainWindow.setMenu(null);
+	var template = [{
+	    label: "Application",
+	    submenu: [
+	        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+	    ]}, {
+	    label: "Edit",
+	    submenu: [
+	        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+	        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+	        { type: "separator" },
+	        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+	        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+	        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+	        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+	    ]}
+	]
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
 	// and load the index.html of the app.
 	mainWindow.loadURL('http://localhost:' + appConfig.serverPort);
