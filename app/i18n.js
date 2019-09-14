73adc56bc7c20ddb5bc061ca591d9023f23a7e13
RemixDev <deezloaderremix@gmail.com>
2019-09-14 18:44:58 +0200

    Added Persian

--- app/i18n.js
+++ app/i18n.js
@@ -2,7 +2,7 @@ var i18n = require('i18n');
 
 i18n.configure({
   // setup some locales - other locales default to en silently
-  locales:['en', 'it', 'de', 'es', 'tr', 'pt-BR', 'uwu'],
+  locales:['en', 'it', 'de', 'es', 'tr', 'pt-BR', 'uwu', 'fa'],
 
   // where to store json files - defaults to './locales' relative to modules directory
   directory: __dirname + '/public/locales',
