7094af134515cc0ea7958abb1b1433cb36af4989
RemixDev <deezloaderremix@gmail.com>
2019-09-14 14:19:53 +0200

    Fixed turkish and added uwu (i'm brain ded right now)

--- app/i18n.js
+++ app/i18n.js
@@ -2,7 +2,7 @@ var i18n = require('i18n');
 
 i18n.configure({
   // setup some locales - other locales default to en silently
-  locales:['en', 'it', 'de', 'es', 'tr', 'pt-BR'],
+  locales:['en', 'it', 'de', 'es', 'tr', 'pt-BR', 'uwu'],
 
   // where to store json files - defaults to './locales' relative to modules directory
   directory: __dirname + '/public/locales',
