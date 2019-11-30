8673b09ccf9be0df2c5866a8a8dfe8797debceba
selfhoster <selfhoster@protonmail.com>
2019-11-30 11:37:58 +1100

    small api and api comments cleanup

--- app/app.js
+++ app/app.js
@@ -111,9 +111,8 @@ app.get('/', function(req, res) {
 });
 
 app.post('/api/download/', function (req, res) {
-	//simple api endpoint that accepts a deezer url, and adds it to download
+	//accepts a deezer url or array of urls, and adds it to download
 	//expecting {"url": "https://www.deezer.com/playlist/xxxxxxxxxx" }
-	//also accepts an array of deezer urls
 	if (Array.isArray(req.body.url)) {
 		for (let x in req.body.url) {
 			clientaddToQueue(req.body.url[x])
@@ -123,16 +122,15 @@ app.post('/api/download/', function (req, res) {
 	}
 
 	res.writeHead(200, { 'Content-Type': 'application/json' });
-	res.end(JSON.stringify({'message': 'Added to Queue'}));
+	res.end(JSON.stringify({'Message': 'Added to Queue'}));
 });
 
 app.post('/api/search/', function (req, res) {
-	//simple api endpoint that accepts a mode (as a key) and a search strings, returns Deezer JSON
+	//accepts a mode (as a key) and search string, returns Deezer JSON
 	//expecting {"album": "discovery - daft punk"}
-	//Returning Deezer JSON instead of URLs puts the onus of figuring out matches on the interfacing software
 	if (Object.keys(req.body).length == 0) {
 		res.writeHead(400, { 'Content-Type': 'application/json' });
-		res.end(JSON.stringify({"Message": "Empty JSON received"}));
+		res.end(JSON.stringify({"Error": "Empty JSON received"}));
 	} else {
 		let mode = Object.keys(req.body)[0] //"album", playlist, album, artist
 		let searchString = req.body[mode]
